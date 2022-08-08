9a6e1654a24a677a1688cc4978afade171ebf245
Aleksander Litin <78950640+avlitvin73@users.noreply.github.com>
2022-08-08 10:53:04 +0000

    feat: add dataSet

---
+++ src/store/action-creators/categoryActions.ts
@@ -0,0 +1,17 @@
+import { Dispatch } from "redux";
+import { CategoryAction, CategoryActionTypes } from "../../types/category";
+import categories from "../../data/categories.json";
+import database from "../../data/database.json";
+
+const fetchCategories = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(categories), 0);
+  });
+
+export const loadCategories = () => {
+  return async (dispatch: Dispatch<CategoryAction>) => {
+
+    const data = await fetchCategories();
+    dispatch({ type: CategoryActionTypes.SET_CATEGORIES, payload: data });
+  };
+};
