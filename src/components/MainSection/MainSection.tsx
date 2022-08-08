6a4f224dc2a62d9f922e71b856ba97b8a5245ef5
Aleksander Litin <78950640+avlitvin73@users.noreply.github.com>
2022-08-08 10:50:02 +0000

    feat: add TypedUseSelectorHook

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,8 +1,10 @@
 import MultiSelect from "../MultiSelect";
 import Select from "../Select";
+import { useTypedSelector } from "./hooks/useTypedSelector";
 
 
 export function MainSection({ }) {
+  const { categories } = useTypedSelector(state => state.category)
   return (
     <>
       <h1>MainSection component</h1>
